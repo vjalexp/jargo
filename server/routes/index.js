d3fba4bbeab72d39b415c0721c1f727ae2ac7253
alexp <ponomarev@iac.spb.ru>
2017-10-04 18:11:59 +0300

    change db conn string

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 const pg = require('pg');
 const path = require('path');
-const connectionString = process.env.DATABASE_URL || 'postgres://postgres:12345678@localhost:5432/jar' ;
+//const connectionString = process.env.DATABASE_URL || 'postgres://postgres:12345678@localhost:5432/jar' ;
+const connectionString = process.env.DATABASE_URL || 'postgres://ohsvvupcbjpdrb:27e2a067cad22434faef8e1b0c05472759b61ff30233afe467d0a3a0e479eddc@ec2-54-83-58-17.compute-1.amazonaws.com:5432/defh9ecmrmkqh6' ;
 
 router.get('/', (req, res, next) => {
   res.sendFile(path.join(
